e472b515436d91e12f77d35397e96c2fc8ceecd1
Lucas Lima <fox_krb@yahoo.com.br>
2026-08-28 21:45:09 -0300

    Update RegistersTests.swift

--- Tests/intel8086Tests/RegistersTests.swift
+++ Tests/intel8086Tests/RegistersTests.swift
@@ -6,7 +6,6 @@ final class RegistersTests: XCTestCase {
     func testRegisters() {
         let cpu = Intel8086()
 
-        // XCTAssertEqual(cpu.registers.ax.value, 0x1234)
         XCTAssertEqual(cpu.registers.cs.value, 0xffff)
         XCTAssertEqual(cpu.registers.ip.value, 0x0000)
         XCTAssertNil(cpu.registers.ax)
